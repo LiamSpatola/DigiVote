0d3e44e94f1db71f6e18a26ebdd4d39f8a90fe87
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2024-09-14 16:17:09 +1000

    Modified the admin panel to make adding new polls easier

--- digivote/vote/admin.py
+++ digivote/vote/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Poll, Choice
 
 # Register your models here.
-admin.site.register(Poll)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["poll_text"]}),
+        ("Date information", {"fields": ["publish_date"], "classes": ["collapse"]}),
+        ("Poll Status", {"fields": ["poll_open"], "classes": ["collapse"]})
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
+admin.AdminSite.site_header = "DigiVote Administration"
