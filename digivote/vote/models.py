d5e4ec06ad7306c2b7e1f376c14c8dae9212187f
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2024-09-14 15:56:39 +1000

    Added poll and choice models and migrated them to the database

--- digivote/vote/models.py
+++ digivote/vote/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    poll_text = models.CharField(max_length=512)
+    publish_date = models.DateTimeField('date published')
+    poll_open = models.BooleanField(default=True)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
