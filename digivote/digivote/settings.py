3c5215fe3bc426ced54dded46931e4b076d92491
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2024-09-15 22:06:22 +1000

    Fixed a typo in the secret key env variable

--- digivote/digivote/settings.py
+++ digivote/digivote/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KET", default="default-secret-key")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
