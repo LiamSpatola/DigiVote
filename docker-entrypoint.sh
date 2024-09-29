00d278dc0c7f4436912297994c766277dbf4e91d
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2024-09-29 11:19:11 +1000

    Updated docker-entrypoint.sh to only create superuser if user does not already exist

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,11 @@
 cd digivote
 # Migrating the changes to the database
 python3 manage.py migrate
-# Creating a super user with the env variables supplied by the user
-python3 manage.py createsuperuser --no-input --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL}
+
+# Check if superuser exists
+if [ -z "$(python3 manage.py shell -c 'from django.contrib.auth.models import User; User.objects.filter(username=__import__("os").environ["DJANGO_SUPERUSER_USERNAME"]).exists()')" ]; then
+    # Creating a super user with the env variables supplied by the user if user doesn't exist already
+    python3 manage.py createsuperuser --no-input --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL}
+fi
 
 exec "$@"
