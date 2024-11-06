d9ecca5af3e9f51b17c2e3c093c026393dcea721
SPAT01 <SPAT01@stpiusx.nsw.edu.au>
2024-11-07 09:40:21 +1100

    Enlarged the text box in the admin panel for voting instructions

--- digivote/vote/admin.py
+++ digivote/vote/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django import forms
+from django.forms import Textarea
 
 from .models import Candidate, Choice, Election, Poll
 
@@ -16,8 +18,28 @@ class CandidateInline(admin.TabularInline):
     extra = 3
 
 
+class PollAdminForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        fields = "__all__"
+        widgets = {
+            'voting_instructions': Textarea(attrs={'rows': 5, 'cols': 80}),
+        }
+
+
+class ElectionAdminForm(forms.ModelForm):
+    class Meta:
+        model = Election
+        fields = "__all__"
+        widgets = {
+            'voting_instructions': Textarea(attrs={'rows': 5, 'cols': 80}),
+        }
+
+
 @admin.register(Poll)
 class PollAdmin(admin.ModelAdmin):
+    form = PollAdminForm
+
     fieldsets = [
         (None, {"fields": ["poll_text"]}),
         ("Date Information", {"fields": ["publish_date", "open_date", "close_date"]}),
@@ -36,11 +58,13 @@ class PollAdmin(admin.ModelAdmin):
         "id",
     )
     search_fields = ("poll_text",)
-    list_filter = ("publish_date", "poll_open")
+    list_filter = ("publish_date", "poll_open", "results_visible")
 
 
 @admin.register(Election)
 class ElectionAdmin(admin.ModelAdmin):
+    form = ElectionAdminForm
+
     fieldsets = [
         (None, {"fields": ["election_name"]}),
         ("Date Information", {"fields": ["publish_date", "open_date", "close_date"]}),
@@ -62,4 +86,4 @@ class ElectionAdmin(admin.ModelAdmin):
         "id",
     )
     search_fields = ("election_name",)
-    list_filter = ("publish_date", "election_open")
+    list_filter = ("publish_date", "election_open", "results_visible")
